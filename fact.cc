6c9d76ea80d0fdc3590b19857af773deff58afbd
Shashankgarg12 <91365027+Shashankgarg12@users.noreply.github.com>
2021-09-25 12:49:29 +0530

    Add files via upload

---
+++ fact.cc
@@ -0,0 +1,13 @@
+//Shashank Garg btech cs sec k 20011190
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,f=1;
+cout<<"enter the no.";
+cin>>a;
+for (int i=1;i<=a;i++)
+{f=f*i;
+}
+cout<<"factorial is ---> "<<f;
+}
